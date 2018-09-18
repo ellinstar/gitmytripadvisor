fed64c389cafe996d5bd7052c683b22c17daaa67
bobpri <ns0b0927@gmail.com>
2018-09-18 14:14:09 +0900

    로그인,회원가입 오류 수정했습니다~

--- WebContent/scripts/register.js
+++ WebContent/scripts/register.js
@@ -1,8 +1,9 @@
+
 $(function() {
-	generateYears($('#sel-birth'));
+   generateYears($('#sel-birth'));
 
 	var birthSelect = $('#sel-birth').selectmenu();
-	birthSelect.selectmenu('menuWidget').addclass('overflow');
+	birthSelect.selectmenu('menuWidget').addClass('overflow');
 
 	$('#form-register').submit(function(e) {
 		e.preventDefault();
@@ -11,13 +12,13 @@ $(function() {
 
 		var email = $('#inp-email').val();
 		if (!validateEmail(email)) {
-			$("#inp-email").next().html('잘못된 형식입니다.').show();
+			$('#inp-email').next().html('잘못된 형식입니다.').show();
 			return;
 		}
 
 		var password = $('#inp-password').val();
 		if (!validatePassword(password)) {
-			$('#inp-password').next().html('대문자와 숫자가 포함된 푀소 8자의 문자열 이어야합니다').show();
+			$('#inp-password').next().html('대문자와 숫자가 포함된 최소 8자의 문자열 이어야합니다').show();
 			return;
 		}
 
@@ -29,7 +30,7 @@ $(function() {
 
 		var gender = $('input[name="gender"]:checked').val();
 		if (!gender) {
-			$('#inp-female').siblings('.txt-warning').html('필수항목 입니다.').show();
+			$('#inp-female').siblings('.txt-warning').html('필수 항목입니다.').show();
 			return;
 		}
 
@@ -51,15 +52,33 @@ $(function() {
 	});
 });
 
-function generateYears($select){
-	for(var i= 1970; i <=2010; i++){
-		$select.append('<option value="'+i+'">'+i+'</option>');
+function generateYears($select) {
+	for (var i = 1970; i <= 2010; i++) {
+		$select.append('<option value="' + i + '">' + i + '</option>');
 	}
 }
 
 
-function validateEmail(email){
+function validateEmail(email) {
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
 }
 
+function validatePassword(password) {
+	var re = /^(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/;
+	return re.test(password);
+
+}
+
+function submit(email, password, gender, birth) {
+	var params = {
+		email : email,
+		password : password,
+		gender : gender,
+		birth : birth
+	};
+	
+	$.post('some-server-url', params, function(r) {
+		 console.log(r);
+	});
+}
